251636651d6436abbb40dd56bb23650ccad9eb26
cristobal-sanchez <tristobal@gmail.com>
2022-04-18 21:45:26 -0400

    Agrega documentación de métodos de Chapter 2.

--- chapter2.py
+++ chapter2.py
@@ -9,12 +9,15 @@ import numpy as np
 img = cv2.imread('resources/lena.png')
 img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# https://www.tutorialkart.com/opencv/python/opencv-python-gaussian-image-smoothing/
 # Must be odd
 gausian_kernel_size = (7, 7)
 img_blur = cv2.GaussianBlur(img_grey, gausian_kernel_size, 0)
 
+# https://programarfacil.com/blog/vision-artificial/detector-de-bordes-canny-opencv/
 img_canny = cv2.Canny(img, 100, 100)
 
+# https://www.javatpoint.com/opencv-erosion-and-dilation
 kernel = np.ones((5, 5), np.uint8)
 img_dialation = cv2.dilate(img_canny, kernel, iterations=1)
 
